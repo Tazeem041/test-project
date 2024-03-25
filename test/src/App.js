5996dabc451d15d4af1bfb4fa5dad184dca338bd
Tazeem041 <124208833+Tazeem041@users.noreply.github.com>
2024-03-25 20:36:02 +0000

    new users add

--- test/src/App.js
+++ test/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,14 +7,16 @@ function App() {
   <User name="Tazeem" job="UI DEV" location="Hybd"/>
   <User name="Shaik"  job="Engineer" location="Ong"/>
   <User name="Syed"   job="React Dev" location="Guntur"/>
+  <User name="praveen" job="UI DEV" location="Hybd"/>
+  <User name="jayanth"  job="Engineer" location="Ong"/>
+  <User name="raghu"   job="React Dev" location="Guntur"/>
   </section>
-  </div>
+      </div>
   
 }
 
-
   function User(props) {
-    return <div className="user">
+    return <div className="user name">
       <h3>{props.name}</h3>
       <p>job: {props.job}</p>
       <p>location: {props.location}</p>
